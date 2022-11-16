d688b5e496b46ffc5a6aa8f09f005aa881c491af
Akin Ramirez <akin.ramirez@hhotmail.com>
2022-11-16 10:15:14 -0600

    Add url diferencia retardo fijo y tasa fija en spring scheduled

--- src/main/java/com/scheduled/cron/ScheduledApp/service/CronService.java
+++ src/main/java/com/scheduled/cron/ScheduledApp/service/CronService.java
@@ -10,6 +10,9 @@ public class CronService {
      https://crontab.cronhub.io/
      https://www.freeformatter.com/cron-expression-generator-quartz.html
      https://www.baeldung.com/spring-scheduled-tasks
+     https://stackoverflow.com/questions/38842507/whats-the-difference-between-fixed-rate-and-fixed-delay-in-spring-scheduled-ann#:~:text=fixedRate%20%3A%20makes%20Spring%20run%20the,when%20the%20last%20execution%20finishes.
+     https://javahowtos.com/guides/107-spring/321-difference-between-fixedrate-and-fixeddelay-parameter-in-scheduled-in-spring.html
+     https://howtodoinjava.com/spring-core/spring-scheduled-annotation/
      */
     @Scheduled(cron = "*/3 * * * * *")
     //@Scheduled(cron = "@hourly")
