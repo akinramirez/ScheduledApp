0aa6cd1385f8a52c5c48e6fed8be20c949272e6a
Akin Ramirez <akin.ramirez@hhotmail.com>
2022-11-16 07:19:19 -0600

    First commit, add config scheduled

---
+++ src/main/java/com/scheduled/cron/ScheduledApp/ScheduledAppApplication.java
@@ -0,0 +1,13 @@
+package com.scheduled.cron.ScheduledApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scheduled/cron/ScheduledApp/ScheduledAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduled.cron.ScheduledApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
